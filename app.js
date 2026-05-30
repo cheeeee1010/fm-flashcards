c635234aaefc77c9cf42a8e3b1d1453a237cbe6e
Tommy Low <cheeeee1010@gmail.com>
2026-05-30 12:27:40 +0800

    chore: remove hardcoded api key

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const filterItems = document.querySelectorAll('.filter-item');
 
 // Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDnNdB2ID8gx7k6bz7FOTVCtuWO8N4-JyI",
+  apiKey: "REPLACE_WITH_YOUR_API_KEY",
   authDomain: "fm-flashcard.firebaseapp.com",
   projectId: "fm-flashcard",
   storageBucket: "fm-flashcard.firebasestorage.app",
